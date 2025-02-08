1d467876a1dab2c6e53d34bf86a7e325f7964a52
Ethan Lin <midgetlin2003@gmail.com>
2025-02-08 12:50:02 -0800

    Added styling to skip and like button

--- frontend/src/components/LikeButton.jsx
+++ frontend/src/components/LikeButton.jsx
@@ -3,7 +3,7 @@ import Heart from "../assets/heart.svg";
 
 const LikeButton = ({ onClick }) => {
     return (
-        <button onClick={onClick}>
+        <button className="hover:scale-130 transition-transform duration-200 ease-in-out active:scale-95" onClick={onClick}>
             <img src={Heart} className="w-8 h-8"/>
         </button>
     )
--- frontend/src/components/SkipButton.jsx
+++ frontend/src/components/SkipButton.jsx
@@ -3,7 +3,7 @@ import Skip from "../assets/skip.svg";
 
 const SkipButton = ({ onClick }) => {
     return (
-        <button onClick={onClick}>
+        <button className="hover:scale-120 transition-transform duration-200 ease-in-out active:scale-95" onClick={onClick}>
             <img src={Skip} className="w-8 h-8"/>
         </button>
     )
