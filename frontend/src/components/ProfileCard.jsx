50f56cb211c7275a1b6e559f96ee53ee3764e372
Ethan Lin <midgetlin2003@gmail.com>
2025-02-08 12:42:00 -0800

    Readded description and dynamically load grad/job status

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -59,27 +59,44 @@ const ProfileCard = ({ userProfiles }) => {
 
     return (
         !noMoreProfiles ? (
-            <div className="flex flex-col justify-center items-center bg-white shadow-md shadow-gray-300 p-10 rounded-lg m-10">
-                <div className="flex flex-row text-left justify-center items-center w-full space-x-2">
-                    <img src={image} className="h-50 rounded-full mr-8"/>
-                    <div className="">
-                        <h1 className="font-bold text-2xl">
-                            {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].name : "Loading..."}
-                        </h1>
-                        <div className="flex space-x-2 mt-1 text-gray-400">
-                            <span className="font-semibold text-sm">
-                                Undergraduate {/* replace with what they're studying i.e. graduate, undergrad, etc. */}
-                            </span>
-                            <span className="font-semibold text-sm">
-                                {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].age : "Loading..."}
-                            </span>
-                        </div>
-                        <div className="inline-block bg-blue-300 rounded-md px-3 py-1 text-sm font-semibold mt-6">
-                            <span className="text-white">Full time</span>
+            <div className="flex flex-col justify-center items-center bg-white shadow-md shadow-gray-300 p-10 rounded-lg m-10 sm:w-full sm:h-auto md:w-1/4 md:h-auto">
+                <div className="flex flex-col text-left justify-star items-start w-full space-x-2">
+                    <div className="flex flex-row justify-center items-center">
+                        <img src={image} className="h-50 rounded-full mr-8"/>
+                        <div className="">
+                            <h1 className="font-bold text-2xl">
+                                {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].name : "Loading..."}
+                            </h1>
+                            <div className="flex space-x-2 mt-1 text-gray-400">
+                                <span className="font-semibold text-sm">
+                                    {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].grad : "Loading..."}
+                                </span>
+                                <span className="font-semibold text-sm">
+                                    {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].age : "Loading..."}
+                                </span>
+                            </div>
+                            <div className="inline-block bg-blue-300 rounded-md px-3 py-1 text-sm font-semibold mt-6">
+                                <span className="text-white">
+                                    {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].job_status : "Loading..."}
+                                </span>
+                            </div>
                         </div>
                     </div>
+                    <div className="flex flex-wrap flex-1 items-start">
+                        {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].skills.map((item, index) => (
+                            <SkillIcon key={index} text={item}/>
+                        )) : "Loading..."}
+                    </div>
+                    <div className="mt-10">
+                        <h1 className="font-semibold text-lg mb-2">
+                            About Me
+                        </h1>
+                        <span className="font-[500] text-gray-600">
+                            {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].description : "Loading..."}
+                        </span>
+                    </div>
                 </div>
-                <SkillIcon text={"hi"}/>
+                
                 
                 <div className="flex flex-row justify-between w-full items-center mt-6">
                     <SkipButton onClick={handleSkip} />
@@ -87,7 +104,7 @@ const ProfileCard = ({ userProfiles }) => {
                 </div>
             </div>
         ) : (
-            <div className="flex text-3xl font-bold bg-white shadow-md shadow-gray-300 p-20 rounded-lg m-10">
+            <div className="flex w-1/4 text-3xl font-bold bg-white shadow-md shadow-gray-300 p-20 rounded-lg m-10 justify-center">
                 <h1>No More Profiles!</h1>
             </div>
         )
