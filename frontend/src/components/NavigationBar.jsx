31bc476189c151e333ecdd5d22da040e52a06a05
EthanSantos <ethanangelo61@gmail.com>
2025-02-09 18:56:17 -0800

    navbar uses react-dom link instead of href

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import profile from '../assets/user_logo.png';
 
 const NavigationBar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-6 py-3 flex items-center justify-between">
-        <a href="/home" className="flex items-center space-x-2">
+        <Link to="/home" className="flex items-center space-x-2">
           <img
             src="../../public/jollibuilders.png"
             alt="Jollibuilders FIP logo"
@@ -13,14 +14,14 @@ const NavigationBar = () => {
           <h1 className="text-base sm:text-lg font-medium text-gray-900">
             Job <span className="text-gray-600">Connector</span>
           </h1>
-        </a>
-        <a href="/account">
+        </Link>
+        <Link to="/account">
           <img
             src={profile}
             alt="Profile logo"
             className="w-8 h-8 sm:w-10 sm:h-10 rounded-full"
           />
-        </a>
+        </Link>
       </div>
     </nav>
   );
