7985d4ff99648accdd7fa29a641ab39c88a78ff4
Ethan Lin <midgetlin2003@gmail.com>
2025-02-15 20:16:22 -0800

    Added alert for match and correctly displays match name

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -11,7 +11,9 @@ import SkillIcon from './SkillIcon.jsx';
 const ProfileCard = () => {
     const [displayedProfiles, setDisplayedProfiles] = useState([]);
     const [currentIdx, setCurrentIdx] = useState(0);
-    const [noMoreProfiles, setNoMoreProfiles] = useState(true);
+    const [showMatchToast, setShowMatchToast] = useState(false);
+    const [toastProgress, setToastProgress] = useState(100);
+    const [moreProfiles, setMoreProfiles] = useState(true);
 
     const jobStatusColors = {
         "Internship": 'bg-green-300',
@@ -25,7 +27,7 @@ const ProfileCard = () => {
             if (prevIdx + 1 < displayedProfiles.length) {
                 return prevIdx + 1;
             }
-            setNoMoreProfiles(true);
+            setMoreProfiles(true);
             return 0;
         });
     }
@@ -53,14 +55,13 @@ const ProfileCard = () => {
 
             const data = await response.json();
             if (response.ok) {
+                if(data.message === "Match detected") {
+                    setShowMatchToast(true);
+                    setToastProgress(100);
+                    setTimeout(() => setShowMatchToast(false), 3000);
+                }
                 console.log("Like successful:", data);
-                setCurrentIdx((prevIdx) => {
-                    if (prevIdx + 1 < displayedProfiles.length) {
-                        return prevIdx + 1;
-                    }
-                    setNoMoreProfiles(true);
-                    return 0;
-                });
+                
             } else {
                 console.error("Like failed:", data.message);
             }
@@ -86,60 +87,99 @@ const ProfileCard = () => {
     
     useEffect(() => {
         getUsersToDisplay();
-        setNoMoreProfiles(false);
+        setMoreProfiles(false);
         console.log(displayedProfiles);
     }, [])
 
     useEffect(() => {
-        console.log("reload");
-        console.log(displayedProfiles[currentIdx]?.job_status);
-    }, [currentIdx]);
-    //might not need
+        if (showMatchToast) {
+            const interval = setInterval(() => {
+                setToastProgress((prev) => {
+                    if (prev <= 0) {
+                        clearInterval(interval);
+                        return 0;
+                    }
+                    return prev - 1;
+                });
+            }, 30); 
+
+            return () => clearInterval(interval);
+        }
+        if (!showMatchToast) {
+            setCurrentIdx((prevIdx) => {
+                if (prevIdx + 1 < displayedProfiles.length) {
+                    return prevIdx + 1;
+                }
+                setMoreProfiles(true);
+                return 0;
+            });
+        }
+    }, [showMatchToast]);
 
     //add grad year
     return (
-        !noMoreProfiles ? (
-            <div className="flex flex-col justify-center items-center bg-white shadow-md shadow-gray-300 p-10 rounded-lg m-10 sm:w-full sm:h-auto md:w-1/4 md:h-auto">
-                <div className="flex flex-col text-left justify-star items-start w-full space-x-2">
-                    <div className="flex flex-row justify-center items-center">
-                        <img src={image} className="h-50 rounded-full mr-8"/>
-                        <div className="">
-                            <h1 className="font-bold text-2xl">
-                                {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].name : "Loading..."}
-                            </h1>
-                            <div className="flex space-x-2 mt-1 text-gray-400">
-                                <span className="font-semibold text-sm">
-                                    {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].grad : "Loading..."}
-                                </span>
-                                <span className="font-semibold text-sm">
-                                    {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].age : "Loading..."}
-                                </span>
-                            </div>
-                            <div className={`inline-block rounded-md px-3 py-1 text-sm font-semibold mt-6 ${jobStatusColors[displayedProfiles[currentIdx]?.job_status]}`}>
-                                <span className="text-gray-700">
-                                    {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].job_status : "Loading..."}
-                                </span>
+        moreProfiles ? (
+            <div className="flex flex-col w-full items-center justify-center">
+                {showMatchToast && (
+                    <div className="fixed top-10 bg-green-500 text-white w-auto px-4 py-2 rounded shadow-lg shadow-gray-300 z-20">
+                        Match with {displayedProfiles[currentIdx].name}!
+                        <button
+                            className="ml-2 text-sm text-white font-bold"
+                            onClick={() => setShowMatchToast(false)}
+                        >
+                            ✕
+                        </button>
+                        <div
+                            className="h-1 bg-white rounded mt-2"
+                            style={{
+                                width: `${toastProgress}%`,
+                                transition: "width 0.03s linear",
+                            }}
+                        />
+                    </div>
+                )}
+                <div className="bg-white shadow-md shadow-gray-300 p-10 rounded-lg m-10 sm:w-full sm:h-auto md:w-1/4 md:h-auto">
+                    <div className="flex flex-col text-left justify-star items-start w-full space-x-2">
+                        <div className="flex flex-row justify-center items-center">
+                            <img src={image} className="h-50 rounded-full mr-8"/>
+                            <div className="">
+                                <h1 className="font-bold text-2xl">
+                                    {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].name : "Loading..."}
+                                </h1>
+                                <div className="flex space-x-2 mt-1 text-gray-400">
+                                    <span className="font-semibold text-sm">
+                                        {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].grad : "Loading..."}
+                                    </span>
+                                    <span className="font-semibold text-sm">
+                                        {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].age : "Loading..."}
+                                    </span>
+                                </div>
+                                <div className={`inline-block rounded-md px-3 py-1 text-sm font-semibold mt-6 ${jobStatusColors[displayedProfiles[currentIdx]?.job_status]}`}>
+                                    <span className="text-gray-700">
+                                        {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].job_status : "Loading..."}
+                                    </span>
+                                </div>
                             </div>
                         </div>
+                        <div className="flex flex-wrap flex-1 items-start">
+                            {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].skills.map((item, index) => (
+                                <SkillIcon key={index} text={item}/>
+                            )) : "Loading..."}
+                        </div>
+                        <div className="mt-10">
+                            <h1 className="font-semibold text-lg mb-2">
+                                About Me
+                            </h1>
+                            <span className="font-[500] text-gray-600">
+                                {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].description : "Loading..."}
+                            </span>
+                        </div>
                     </div>
-                    <div className="flex flex-wrap flex-1 items-start">
-                        {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].skills.map((item, index) => (
-                            <SkillIcon key={index} text={item}/>
-                        )) : "Loading..."}
+                    
+                    <div className="flex flex-row justify-between w-full items-center mt-6">
+                        <SkipButton onClick={handleSkip} />
+                        <LikeButton onClick={handleLike} />
                     </div>
-                    <div className="mt-10">
-                        <h1 className="font-semibold text-lg mb-2">
-                            About Me
-                        </h1>
-                        <span className="font-[500] text-gray-600">
-                            {displayedProfiles[currentIdx] ? displayedProfiles[currentIdx].description : "Loading..."}
-                        </span>
-                    </div>
-                </div>
-                
-                <div className="flex flex-row justify-between w-full items-center mt-6">
-                    <SkipButton onClick={handleSkip} />
-                    <LikeButton onClick={handleLike} />
                 </div>
             </div>
         ) : (
