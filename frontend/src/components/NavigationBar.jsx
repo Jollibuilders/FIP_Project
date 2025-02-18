db0c9d8e7de3a334d9c129be17109b555af7a3b4
Tawann Alvarez <htalvare@uci.edu>
2025-02-18 15:40:18 -0800

    went back to fix errors

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -66,11 +66,7 @@ const NavigationBar = () => {
             <span>Edit Profile</span>
             </button>
             </Link>
-            <Link to="/logout" onClick={() => {
-              setDropdownOpen(false)
-              setUser(null) 
-              auth.signOut()
-            }}>
+            <Link to="/logout" onClick={() => setDropdownOpen(false)}>
             <button className="w-full flex items-center gap-2 px-4 py-2 hover:bg-gray-100">
             <IoLogOutOutline className="w-5 h-5 text-gray-600" />
             <span>Log Out</span>
