4a513382766adb3ebed98c0e22d0ecf3a9859065
SebastianCapuyanNSJ <capuyanj@uci.edu>
2025-02-18 10:24:51 -0800

    added functions to handle logout and edit buttos

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,7 +1,41 @@
 import { Link } from 'react-router-dom';
 import profile from '../assets/user_logo.png';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom'; 
+import { auth } from "./firebase"; 
 
 const NavigationBar = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => 
+  {
+    const unsubscribe = auth.onAuthStateChanged((user) => 
+      {
+        setUser(user);
+      });
+      return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => 
+  {
+    try 
+    {
+      await signOut(auth);
+      console.log("User logged out");
+      setUser(null);
+    } 
+    catch (err) 
+    {
+      console.error('Error logging out:', err);
+    }
+  };
+
+  const handleEdit = () =>
+    {
+      navigate('/profile-setup');
+    }
+
   return (
     <nav className="fixed top-0 left-0 w-full bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-6 py-3 flex items-center justify-between">
