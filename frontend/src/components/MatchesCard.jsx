dbee8eaacbf228da4c3b018d5a6e5723408bac56
SebastianCapuyanNSJ <capuyanj@uci.edu>
2025-05-06 02:35:14 -0700

    Updated MatchesCard Component to link to Profile Page of Indiviual User

--- frontend/src/components/MatchesCard.jsx
+++ frontend/src/components/MatchesCard.jsx
@@ -3,27 +3,27 @@ import image from '../assets/test_image.jpg';
 import { Link } from 'react-router-dom';
 
 const MatchesCard = ({ matchName, date, likedUserId }) => {
-    const formattedDate = date? 
+    const formattedDate = date ? 
         new Date(date._seconds * 1000).toLocaleDateString("en-US", { 
             month: "long",
             day: "numeric",
         }) 
         : "No date available";
+
     return (
         <div className="flex flex-row items-center w-1/4 mb-2">
-            <img src={image} className="h-20 rounded-full mr-4"/>
+            <img src={image} alt="Match profile" className="h-20 w-20 object-cover rounded-full mr-4" />
             <div className="flex flex-col items-start w-full border-b border-gray-300 py-4">
                 <div className="flex flex-row justify-between items-center w-full">
-                <Link 
-                        to={`/profile/${likedUserId}`} 
-                        className="font-semibold text-black-600 hover:underline"
+                    <Link 
+                        to={`/profile=${likedUserId}`} 
+                        className="font-semibold text-black hover:underline"
                     >
                         {matchName}
                     </Link>
-                    <span className="font-medium text-right">{formattedDate}</span>
-                    {/* matched date */}
+                    <span className="font-medium text-sm text-right">{formattedDate}</span>
                 </div> 
-                <p className="text-sm max-w-md text-left pt-2">
+                <p className="text-sm max-w-md text-left pt-2 text-gray-600">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
             </div>
